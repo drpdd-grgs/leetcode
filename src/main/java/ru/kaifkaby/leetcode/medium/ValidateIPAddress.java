b69d7b6877c9e8e3d834bc03134963814713f530
drpdd-grgs <therealloathing@gmail.com>
2025-06-22 03:37:43 +0400

    optimize validate ip address

--- src/main/java/ru/kaifkaby/leetcode/medium/ValidateIPAddress.java
+++ src/main/java/ru/kaifkaby/leetcode/medium/ValidateIPAddress.java
@@ -8,6 +8,7 @@ public class ValidateIPAddress {
         }
         char[] ip = queryIP.toCharArray();
 
+        char c;
         int i = 0;
         int pos = 0;
         int ipv4slice = 0;
@@ -16,8 +17,9 @@ public class ValidateIPAddress {
         boolean mayBeIPv6 = false;
 
         while (i < queryIP.length()) {
-            if (ip[i] == 46) {
-                if (ipv4slice < 256 && separatorCount < 3 && pos != 0) {
+            c = (char) (ip[i] | 0x20);
+            if (c == '.') {
+                if (ipv4slice < 256 && separatorCount != 3 && pos != 0) {
                     ipv4slice = 0;
                     pos = 0;
                     i++;
@@ -38,15 +40,15 @@ public class ValidateIPAddress {
                 return "Neither";
             }
 
-            if (pos > 2 || ip[i] > 57 || ip[i] < 48) {
+            if (pos > 2 || c > '9' || c < '0') {
                 if (separatorCount == 0) {
-                    if ((ip[i] > 96 && ip[i] < 103) || (ip[i] > 64 && ip[i] < 71) || (ip[i] < 58 && ip[i] > 47)) {
+                    if ((c >= 'a' && c <= 'f') || (c >= '0' && c <= '9')) {
                         mayBeIPv6 = true;
                         i++;
                         pos++;
                         break;
                     }
-                    if (ip[i] == 58) {
+                    if (c == 58) {
                         separatorCount++;
                         mayBeIPv6 = true;
                         i++;
@@ -57,7 +59,7 @@ public class ValidateIPAddress {
                 return "Neither";
             }
 
-            ipv4slice = (ipv4slice * 10) + ip[i] - 48;
+            ipv4slice = (ipv4slice * 10) + c - 48;
             pos++;
             i++;
         }
@@ -71,7 +73,8 @@ public class ValidateIPAddress {
         }
 
         while (i < queryIP.length()) {
-            if (ip[i] == 58 && pos != 0) {
+            c = (char) (ip[i] | 0x20);
+            if (c == ':' && pos != 0) {
                 if (separatorCount == 7) {
                     return "Neither";
                 }
@@ -83,7 +86,7 @@ public class ValidateIPAddress {
             if (pos > 3) {
                 return "Neither";
             }
-            if (!(ip[i] < 58 && ip[i] > 47) && !(ip[i] > 96 && ip[i] < 103) && !(ip[i] > 64 && ip[i] < 71)) {
+            if (!(c >= 'a' && c <= 'f') && !(c >= '0' && c <= '9')) {
                 return "Neither";
             }
             i++;
--- src/test/java/ru/kaifkaby/leetcode/medium/ValidateIPAddressTest.java
+++ src/test/java/ru/kaifkaby/leetcode/medium/ValidateIPAddressTest.java
@@ -67,6 +67,11 @@ class ValidateIPAddressTest {
 
     @Test
     void test_13() {
-        Assertions.assertEquals("IPv6", new ValidateIPAddress().validIPAddress("192.23:0:0:0:0:0:0"));
+        Assertions.assertEquals("Neither", new ValidateIPAddress().validIPAddress("192.23:0:0:0:0:0:0"));
+    }
+
+    @Test
+    void test_14() {
+        Assertions.assertEquals("Neither", new ValidateIPAddress().validIPAddress("20EE:FGb8:85a3:0:0:8A2E:0370:7334"));
     }
 }
