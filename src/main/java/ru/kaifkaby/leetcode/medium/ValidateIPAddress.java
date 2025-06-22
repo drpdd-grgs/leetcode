6ae24b1919e7bcbcb05bb9905a2022ad96eacdbf
drpdd-grgs <therealloathing@gmail.com>
2025-06-22 04:12:19 +0400

    optimize validate ip address

--- src/main/java/ru/kaifkaby/leetcode/medium/ValidateIPAddress.java
+++ src/main/java/ru/kaifkaby/leetcode/medium/ValidateIPAddress.java
@@ -1,70 +1,86 @@
 package ru.kaifkaby.leetcode.medium;
 
+/**
+ * <a href="https://leetcode.com/problems/validate-ip-address">Validate IP Address - LeetCode</a>
+ */
 public class ValidateIPAddress {
 
     public String validIPAddress(String queryIP) {
+
+        // Minimum length of ip address (0.0.0.0).
         if (queryIP.length() < 7) {
             return "Neither";
         }
 
-        char c;
+        // String iterator.
         int i = 0;
-        int pos = 0;
-        int ipv4slice = 0;
-        int separatorCount = 0;
+        // Current character at iteration.
+        char c;
 
-        boolean mayBeIPv6 = false;
+        // Slice iterator (slice is number/characters between separators).
+        int si = 0;
+        // Number in slice (for ipv4 validation only).
+        int sl = 0;
+        // Count of separators (dots or colons).
+        int sc = 0;
+
+        // Flag represents, that address is surely not IPv4 but may be IPv6.
+        boolean ms = false;
 
         while (i < queryIP.length()) {
+            // Lowercase.
             c = (char) (queryIP.charAt(i) | 0x20);
+
             if (c == '.') {
-                if (ipv4slice < 256 && separatorCount != 3 && pos != 0) {
-                    ipv4slice = 0;
-                    pos = 0;
+                if (sl < 256 && sc != 3 && si != 0) {
+                    sl = 0;
+                    si = 0;
                     i++;
-                    separatorCount++;
+                    sc++;
                     continue;
                 } else {
                     return "Neither";
                 }
             }
 
-            if (pos == 1 && ipv4slice == 0) {
-                if (separatorCount == 0) {
-                    mayBeIPv6 = true;
+            // Starts with 0.
+            if (si == 1 && sl == 0) {
+                if (sc == 0) {
+                    ms = true;
                     i++;
-                    pos++;
+                    si++;
                     break;
                 }
                 return "Neither";
             }
 
-            if (pos > 2 || c > '9' || c < '0') {
-                if (separatorCount == 0) {
+            if (si > 2 || c > '9' || c < '0') {
+                if (sc == 0) {
                     if ((c >= 'a' && c <= 'f') || (c >= '0' && c <= '9')) {
-                        mayBeIPv6 = true;
+                        ms = true;
                         i++;
-                        pos++;
+                        si++;
                         break;
                     }
-                    if (c == 58) {
-                        separatorCount++;
-                        mayBeIPv6 = true;
+                    if (c == ':') {
+                        sc++;
+                        ms = true;
                         i++;
-                        pos = 0;
+                        si = 0;
                         break;
                     }
                 }
                 return "Neither";
             }
 
-            ipv4slice = (ipv4slice * 10) + c - 48;
-            pos++;
+            // Add number character to slice.
+            sl = (sl * 10) + c - 48;
+            si++;
             i++;
         }
 
-        if (!mayBeIPv6) {
-            if (separatorCount == 3 && pos != 0 && ipv4slice < 256) {
+        if (!ms) {
+            if (sc == 3 && si != 0 && sl < 256) {
                 return "IPv4";
             } else {
                 return "Neither";
@@ -72,27 +88,28 @@ public class ValidateIPAddress {
         }
 
         while (i < queryIP.length()) {
+            // Lowercase.
             c = (char) (queryIP.charAt(i) | 0x20);
-            if (c == ':' && pos != 0) {
-                if (separatorCount == 7) {
+            if (c == ':' && si != 0) {
+                if (sc == 7) {
                     return "Neither";
                 }
-                separatorCount++;
-                pos = 0;
+                sc++;
+                si = 0;
                 i++;
                 continue;
             }
-            if (pos > 3) {
+            if (si > 3) {
                 return "Neither";
             }
             if (!(c >= 'a' && c <= 'f') && !(c >= '0' && c <= '9')) {
                 return "Neither";
             }
             i++;
-            pos++;
+            si++;
         }
 
-        if (separatorCount == 7 && pos != 0) {
+        if (sc == 7 && si != 0) {
             return "IPv6";
         } else {
             return "Neither";
